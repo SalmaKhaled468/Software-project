f187b35be7e1fc0e3b23cc697b27889edf67f292
SalmaK-0829 <salma.aly@gaf.ac>
2025-12-09 22:47:58 +0200

    ehr class

--- src/EHR.java
+++ src/EHR.java
@@ -1,2 +1,58 @@
 public class EHR {
+    private int id;
+    private String diagnosis;
+    private String treatment;
+    private Patient patient;
+    private Provider provider;
+
+    public EHR(int id, String diagnosis, String treatment,
+               Patient patient, Provider provider) {
+        this.id = id;
+        this.diagnosis = diagnosis;
+        this.treatment = treatment;
+        this.patient = patient;
+        this.provider = provider;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDiagnosis() {
+        return diagnosis;
+    }
+
+    public void setDiagnosis(String diagnosis) {
+        this.diagnosis = diagnosis;
+    }
+
+    public String getTreatment() {
+        return treatment;
+    }
+
+    public void setTreatment(String treatment) {
+        this.treatment = treatment;
+    }
+
+    public Patient getPatient() {
+        return patient;
+    }
+
+    public void setPatient(Patient patient) {
+        this.patient = patient;
+    }
+
+    public Provider getProvider() {
+        return provider;
+    }
+
+    public void setProvider(Provider provider) {
+        this.provider = provider;
+    }
+
+
 }
